e318d51f5835b1d75ed197e1e2d956424d8730e8
Luis Alberto Morales Tueme <36804788+luismtueme@users.noreply.github.com>
2024-08-09 12:40:39 -0400

    Add files via upload

---
+++ mavenProjectTest/src/test/java/com/studentApp/examples/app.java
@@ -0,0 +1,13 @@
+package com.studentApp.examples;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class app {
+
+    @Test
+    public void returnTrue(){
+        assertTrue(true);
+    }
+}
